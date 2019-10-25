49dfec2cf2c2a0d72d04f8fc81a14b968746be8f
Adam Bahr <adam@karmka.com>
2019-10-25 17:28:41 -0400

    Updated license

--- lib/src/spannable_text.dart
+++ lib/src/spannable_text.dart
@@ -1,3 +1,22 @@
+/*
+ * Karmka Text Editor
+ * Copyright 2019 Adam Bahr.
+ * https://github.com/TutOsirisOm/karmka_text_editor/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * This project uses modified source code from https://github.com/namhyun-gu/flutter_rich_text_editor, copyright Namhyun Gu 2019, licensed under the Apache 2.0 license.
+ */
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
